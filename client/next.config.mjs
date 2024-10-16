22af2dc4b8714e6ad1fef95346e1be32d6a89915
Myxogastria0808 <r.rstudio.c@gmail.com>
2024-10-16 22:55:02 +0900

    wip

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   output: "standalone",
   images: {
-    domains: ["127.0.0.1:5000", "pub-05386dd671a54d04958b914542c641bb.r2.dev"],
+    domains: ["127.0.0.1:5000", "pub-236a41428aff4b1abfe53652a32e2b13.r2.dev"],
   },
 };
 
